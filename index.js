d6f09b39a3557414f5eff0d0f950b7281a9c8c9f
Никита Кочетков <n.kochetkov@isimplelab.com>
2017-04-19 18:21:46 +0300

    Start create lol command

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var TelegramBot = require('node-telegram-bot-api');
+let request = require('request');
+let https = require('https');
+let TelegramBot = require('node-telegram-bot-api');
 
-var token = '340829957:AAH2nqKteoElJw8tWQh3SJTCBIiQIixuTew';
-var botOptions = {
+let token = '340829957:AAH2nqKteoElJw8tWQh3SJTCBIiQIixuTew';
+let botOptions = {
     polling: true
 };
-var kittenArray = [
+let lolApiKey = 'RGAPI-1fc5b64a-0dec-433c-9cb6-93b34d30c663';
+let kittenArray = [
     'https://static1.squarespace.com/static/54e8ba93e4b07c3f655b452e/t/56c2a04520c64707756f4267/1455596221531/',
     'http://i.telegraph.co.uk/multimedia/archive/02830/cat_2830677b.jpg',
     'http://dreamatico.com/data_images/kitten/kitten-3.jpg',
@@ -12,7 +15,7 @@ var kittenArray = [
     'http://weknowyourdreams.com/images/kitten/kitten-01.jpg',
     'http://static.boredpanda.com/blog/wp-content/uploads/2016/08/cute-kittens-69-57b32c431e8a7__605.jpg'
 ]
-var bot = new TelegramBot(token, botOptions);
+let bot = new TelegramBot(token, botOptions);
 
 bot.getMe().then(function(me)
 {
@@ -23,25 +26,51 @@ bot.getMe().then(function(me)
 
 bot.on('text', function(msg)
 {
-    var messageChatId = msg.chat.id;
-    var messageText = msg.text;
-    var messageDate = msg.date;
-    var messageUsr = msg.from.username;
+    let messageChatId = msg.chat.id;
+    let messageText = msg.text;
+    let messageDate = msg.date;
+    let messageUsr = msg.from.username;
 
     if (messageText === 'Привет') {
         sendMessageByBot(messageChatId, 'Привет киса, я тебя очень очень сильно люблю <3');
     }
     if (messageText === 'Kitten') {
-        var rand = Math.floor(Math.random() * (kittenArray.length - 0 + 1)) + 0;
+        let rand = Math.floor(Math.random() * (kittenArray.length - 0 + 1));
         if(kittenArray[rand] !== undefined)
         bot.sendPhoto(messageChatId,kittenArray[rand]);
         else {
             bot.sendPhoto(messageChatId,kittenArray[3]);
         }
     }
+    if (messageText === 'L'){
+        let answer = getLeagueData('fiercekilla');
+        sendMessageByBot(messageChatId, 'Вы играли за ' + answer.champName + '. Победа:  ' + answer.win);
+    }
 
     console.log(msg);
 });
+
+function getLeagueData(name) {
+    let sumId;
+    request('https://ru.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + name + '?api_key=RGAPI-1fc5b64a-0dec-433c-9cb6-93b34d30c663', function (error, response, body) {
+        console.log('SummonerId:', JSON.parse(body).id); // Print the HTML for the Google homepage.
+        sumId =  JSON.parse(body).id;
+        request('https://ru.api.riotgames.com/api/lol/RU/v1.3/game/by-summoner/' + sumId + '/recent?api_key=RGAPI-1fc5b64a-0dec-433c-9cb6-93b34d30c663', function (error, response, body) {
+            let matchList = JSON.parse(body).games;
+            let win = matchList[0].stats['win'];
+            request('https://ru.api.riotgames.com/lol/static-data/v3/champions/' + matchList[0].championId + '?champData=info&api_key=RGAPI-1fc5b64a-0dec-433c-9cb6-93b34d30c663', function (error, response, body) {
+                let name = JSON.parse(body).name;
+                let data = {
+                    win: win,
+                    champName: name
+                };
+                console.log(data);
+                return data;
+            })
+        });
+    });
+
+}
 
 function sendMessageByBot(aChatId, aMessage)
 {
